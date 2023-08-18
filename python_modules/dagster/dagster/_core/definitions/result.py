8b520e768e8ad271d6453c2f0d466d87e3374414
alangenfeld <alex@elementl.com>
2023-08-18 10:51:51 -0500

    add MaterializeResult

---
+++ python_modules/dagster/dagster/_core/definitions/result.py
@@ -0,0 +1,40 @@
+from typing import NamedTuple, Optional
+
+from .events import (
+    AssetKey,
+    CoercibleToAssetKey,
+)
+from .metadata import MetadataUserInput
+
+
+class MaterializeResult(
+    NamedTuple(
+        "_MaterializeResult",
+        [
+            ("asset_key", Optional[AssetKey]),
+            ("metadata", Optional[MetadataUserInput]),
+        ],
+    )
+):
+    """An object representing a successful materialization of an asset. These can be returned from
+    @asset and @multi_asset decorated functions to pass metadata or specify specific assets were
+    materialized.
+
+    Attributes:
+        asset_key (Optional[AssetKey]): Optional in @asset, required in @multi_asset to discern which asset this refers to.
+        metadata (Optional[MetadataUserInput]): Metadata to record with the corresponding AssetMaterialization event.
+    """
+
+    def __new__(
+        cls,
+        *,  # enforce kwargs
+        asset_key: Optional[CoercibleToAssetKey] = None,
+        metadata: Optional[MetadataUserInput] = None,
+    ):
+        asset_key = AssetKey.from_coercible(asset_key) if asset_key else None
+
+        return super().__new__(
+            cls,
+            asset_key=asset_key,
+            metadata=metadata,  # check?
+        )
